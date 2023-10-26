669b0c6cdb581f8428464aa253ff0eed50503525
Sameekksshhaa <sameeksha.gce22@sot.pdpu.ac.in>
2023-10-26 11:26:16 +0530

    added more files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Tilt} from "react-tilt";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -7,12 +7,11 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full max-w-7xl mx-auto relative z-0'>
+  <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card flex flex-row' 
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
     >
       <div
         options={{
@@ -35,28 +34,33 @@ const ServiceCard = ({ index, title, icon }) => (
     </motion.div>
   </Tilt>
 );
+
 const About = () => {
   return (
     <>
-      <motion.div variants = {textVariant()}>
+      <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview</h2>
-      </motion.div> 
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
       <motion.p
-      variants={fadeIn("", "", 0.1, 1)}
-      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        I'm a skilled frontend developer with experience in html, css and javascript and in expertise in framework like React, Tailwind css, Nodjs, Threejs. I'm quick learner and good at team work to create efficient, scalable and user friendly solution that solve real-world problem. Let's work together to bring your ideas to life! 
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
-          <ServiceCard key={service.title}
-          index = {index} {...service} />
-          ))}
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper(About, "about")
+export default SectionWrapper(About, "about");
